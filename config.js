46c787675dcf01bd08c753838985c2d87c2fdf47
Rachel Chen <me@rachelchen.me>
2016-04-29 01:38:01 -0700

    user configurable to config.json

--- config.js
+++ config.js
@@ -3,16 +3,8 @@ module.exports = {
 	cluster: {
 		basePort: 2000
 	},
-	rethinkdb: {
-		host: '127.0.0.1',
-		port: 28015,
-        authKey: "",
-        db: "dermail"
-	},
-	'redisQ': {
-		host: '127.0.0.1',
-		port: 6379
-	},
+	rethinkdb: require('./config.json').rethinkdb,
+	'redisQ': require('./config.json').redisQ,
 	'Qconfig': {
 		attempts: 10,
 		backoff: {
