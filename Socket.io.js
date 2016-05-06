360aec62c022a6c95345535fcf6c84b5e095d064
Rachel Chen <me@rachelchen.me>
2016-05-06 12:42:42 -0700

    io.set origins

--- Socket.io.js
+++ Socket.io.js
@@ -5,6 +5,7 @@ r.connect(config.rethinkdb).then(function(conn) {
 	r.conn = conn;
 	var port = config.cluster.basePort - 1;
 	var io = require('socket.io')(port);
+	io.set('origins', '*');
 	require('./lib/socket')(io, r, config);
 
 	console.log('Process ' + process.pid + ' is listening on port ' + port + ' to incoming Socket.io connections.')
