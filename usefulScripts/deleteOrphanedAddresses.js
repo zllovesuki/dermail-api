ee109ebaf34e31a4065622941af264dc5043a91f
Rachel Chen <me@rachelchen.me>
2016-06-02 22:50:21 -0700

    scripts to delete orphaned addresses

---
+++ usefulScripts/deleteOrphanedAddresses.js
@@ -0,0 +1,41 @@
+var r = require('rethinkdb'),
+	config = require('../config'),
+	helper = require('../lib/helper'),
+	Promise = require('bluebird');
+
+var deleted = [];
+
+r.connect(config.rethinkdb).then(function(conn) {
+	r.conn = conn;
+	return r
+	.table('addresses')
+	.pluck('addressId')
+	.map(function(doc) {
+		return {
+			addressId: doc('addressId'),
+			count: r
+			.table('messages')
+			.pluck('to', 'from')
+			.filter(function(_doc) {
+				return _doc('from').contains(doc('addressId')).or(_doc('to').contains(doc('addressId')))
+			})
+			.count()
+		}
+	})
+	.run(r.conn)
+	.then(function(cursor) {
+		return cursor.toArray();
+	})
+	.then(function(results) {
+		return Promise.map(results, function(result) {
+			if (result.count === 0) {
+				deleted.push(result.addressId)
+				return r.table('addresses').get(result.addressId).delete().run(r.conn);
+			}
+		})
+	})
+	.then(function() {
+		console.log(deleted);
+		conn.close();
+	})
+});
