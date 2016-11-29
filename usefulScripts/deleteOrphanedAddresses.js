4df90213a3808cb00fd532b3e305aa6e57791d5a
Rachel Chen <me@rachelchen.me>
2016-11-28 22:32:57 -0800

    faster deleteOrphanedAddresses

--- usefulScripts/deleteOrphanedAddresses.js
+++ usefulScripts/deleteOrphanedAddresses.js
@@ -7,47 +7,54 @@ var deleted = [];
 
 r.connect(config.rethinkdb).then(function(conn) {
 	r.conn = conn;
-	return r
-	.table('addresses')
-	.pluck('addressId', 'aliasOf')
-	.map(function(doc) {
-		return {
-			addressId: doc('addressId'),
-			alias: r.branch(doc.hasFields('aliasOf'), true, false),
-			count: r
-			.table('messages')
-			.pluck('to', 'from', 'cc', 'bcc')
-            .map(function(row) {
-                return row.merge(function(doc) {
-        			return {
-                        cc: r.branch(doc.hasFields('cc'), doc('cc'), []),
-        				bcc: r.branch(doc.hasFields('bcc'), doc('bcc'), [])
-        			}
-        		})
+    return Promise.all([
+        r.table('messages')
+        .pluck('to', 'from', 'cc', 'bcc')
+        .map(function(row) {
+            return row.merge(function(doc) {
+                return {
+                    cc: r.branch(doc.hasFields('cc'), doc('cc'), []),
+                    bcc: r.branch(doc.hasFields('bcc'), doc('bcc'), [])
+                }
             })
-			.filter(function(_doc) {
-				return _doc('from').contains(doc('addressId'))
-                    .or(_doc('to').contains(doc('addressId')))
-                    .or(_doc('cc').contains(doc('addressId')))
-                    .or(_doc('bcc').contains(doc('addressId')))
-			})
-			.count()
-		}
-	})
-	.run(r.conn)
-	.then(function(cursor) {
-		return cursor.toArray();
-	})
-	.then(function(results) {
-		return Promise.map(results, function(result) {
-			if (result.count === 0 && result.alias === false) {
-				deleted.push(result.addressId)
-				return r.table('addresses').get(result.addressId).delete().run(r.conn);
-			}
-		})
-	})
-	.then(function() {
-		console.log(deleted);
-		conn.close();
-	})
+        })
+        .run(r.conn)
+        .then(function(cursor) {
+            return cursor.toArray()
+        }),
+        r.table('addresses')
+    	.pluck('addressId', 'aliasOf')
+    	.map(function(doc) {
+    		return {
+    			addressId: doc('addressId'),
+    			alias: r.branch(doc.hasFields('aliasOf'), true, false)
+            }
+        })
+        .run(r.conn)
+        .then(function(cursor) {
+            return cursor.toArray();
+        })
+    ]).spread(function(messages, addresses) {
+        var bigArray = [];
+        messages.forEach(function(message) {
+            bigArray = bigArray.concat(message.to);
+            bigArray = bigArray.concat(message.from);
+            bigArray = bigArray.concat(message.cc);
+            bigArray = bigArray.concat(message.bcc);
+        })
+        bigArray = bigArray.filter(function(elem, index, self) {
+            return index == self.indexOf(elem)
+        })
+        addresses = addresses.map(function(address) {
+            if (address.alias === false) return address.addressId;
+            else return false;
+        }).filter(Boolean).filter(function(address) {
+            return bigArray.indexOf(address) < 0;
+        })
+        return r.table('addresses').getAll(r.args(addresses)).delete().run(r.conn)
+        .then(function() {
+    		console.log(addresses);
+    		conn.close();
+    	})
+    })
 });
