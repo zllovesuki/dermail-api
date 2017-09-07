838564d9b39a9f6885a1b3e5faa98bd2f6629715
Rachel Chen <me@rachelchen.me>
2017-09-06 18:52:43 -0700

    remove timer based Probe since workers are not singleton

--- Probe.js
+++ Probe.js
@@ -1,6 +1,4 @@
-var Queue = require('rethinkdb-job-queue'),
-    Promise = require('bluebird'),
-    r = require('rethinkdb'),
+var Promise = require('bluebird'),
 	config = require('./config'),
     bunyan = require('bunyan'),
 	stream = require('gelf-stream'),
@@ -25,89 +23,39 @@ if (!!config.graylog) {
 
 var endpoint = false;
 var endpointChanged = false;
-var forceExit = false;
-
-var lastMessage = null;
 
 discover().then(function(ip) {
     if (ip !== null) {
-        config.rethinkdb.host = ip
         endpoint = ip;
     }
 
-    var messageQ = new Queue(config.rethinkdb, {
-        name: 'healthCheckQueue'
+    app.use(function(req, res, next) {
+        if (endpointChanged) {
+            return next(new Error('Endpoint changed.'))
+        }
+        discover().then(function(ip) {
+            if (endpoint !== false && ip !== endpoint) {
+                endpointChanged = true;
+                throw new Error('Endpoint changed.')
+            }
+            return res.status(200).send({
+                endpoint: ip
+            })
+        }).catch(function(e) {
+            next(e)
+        })
     });
 
-    var queueCounter = function() {
-        var job = messageQ.createJob({
-            type: 'test'
-        }).setTimeout(1 * 60 * 1000).setRetryMax(0);
-        return messageQ.addJob(job).then(function() {
-            setTimeout(queueCounter, 15 * 1000);
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.send({
+            ok: false,
+            errName: err.name,
+            message: err.message
         });
-    }
+    });
 
-    messageQ.ready().then(function() {
-        return messageQ.reset();
-    }).then(function() {
-        r.connect(config.rethinkdb).then(function(conn) {
-            r.conn = conn;
+    app.listen(1999);
 
-            queueCounter();
-
-            messageQ.on('error', function(e) {
-                log.error({ message: 'Error thrown from Queue', error: '[' + e.name + '] ' + e.message, stack: e.stack })
-                forceExit = true;
-            })
-
-            messageQ.process(function(job, next) {
-                messageQ.removeJob(job);
-                lastMessage = new Date();
-                next();
-            })
-
-        	app.use(function(req, res, next) {
-                if (forceExit) {
-                    return next(new Error('Queue error.'))
-                }
-                if (endpointChanged) {
-                    return next(new Error('Endpoint changed.'))
-                }
-                discover().then(function(ip) {
-                    if (endpoint !== false && ip !== endpoint) {
-                        endpointChanged = true;
-                        throw new Error('Endpoint changed.')
-                    }
-                    if (lastMessage === null) {
-                        return res.status(200).send({})
-                    }
-                    var timeNow = new Date();
-                    if (timeNow.getTime() - lastMessage.getTime() > 30 * 1000) {
-                        throw new Error('Timeout!.')
-                    }
-                    return res.status(200).send({
-                        endpoint: ip,
-                        lastMessage
-                    })
-                }).catch(function(e) {
-                    next(e)
-                })
-        	});
-
-        	app.use(function(err, req, res, next) {
-        		res.status(err.status || 500);
-        		res.send({
-        			ok: false,
-        			errName: err.name,
-        			message: err.message
-        		});
-        	});
-
-            app.listen(1999);
-
-            log.info('Process ' + process.pid + ' is running as an API-Worker-Probe listening on port 1999.');
-
-        })
-    })
+    log.info('Process ' + process.pid + ' is running as an API-Worker-Probe listening on port 1999.');
 })
